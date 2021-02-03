f967b9e3d351b1f9f2d9664e4dd90794ad278cd4
ashiqqqq <ashiq.inform@gmail.com>
2021-02-03 17:56:50 +0800

    Update BudgetList.py
    up to mod 2.4

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,7 +1,15 @@
-def class BudgetList(object):
+class BudgetList(object):
     """docstring for ."""
-    pass
-#    def __init__(self, arg):
-#        super(, self).__init__()
-#        self.arg = arg
-        
+
+    def __init__(self, budegt):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if self.sum_expenses + self.item < self.budget:
+            self.expenses.append(self.item)
+            self.sum_expenses =+ item
+            
