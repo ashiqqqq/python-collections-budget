74ee34be43c2e7ef432f91f0d8a40f15865defef
ashiqqqq <ashiq.inform@gmail.com>
2021-02-03 17:45:07 +0800

    Update FrequentExpenses.py
    Spelling changes for PuralSight

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -5,14 +5,16 @@ import matplotlib.pyplot as plt
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 spending_categories =[]
-for expenses in expenses.list:
-    spending_categories.append(expenses.category)
+for expense in expenses.list:
+    spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
+#print(spending_counter)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
+#print(categories, count)
 
 fig, ax = plt.subplots()
 ax.bar(categories, count)
-ax.set_title('# of purchases by category')
+ax.set_title('# of Purchases by Category')
 plt.show()
